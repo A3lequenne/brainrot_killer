960d87d78ef47d3940caf43c6cf3d7befbe560af
Clément Lequenne <clementlequenne1@gmail.com>
2024-11-21 11:53:59 +0100

    gonna do toogle

--- src/background/background.js
+++ src/background/background.js
@@ -1,15 +1,8 @@
 let settings = {
   running: false, // Not activated by default
-  selectedTime: 15 // 15 minutes by default
+  selectedTime: 0, // 15 minutes by default
+  resetTimerTime: 0 // 10 minutes by default
 };
-
-// FOR TESTING PURPOSES
-
-if (settings.selectedTime === 0) {
-  settings.selectedTime = 0.2;
-}
-
-// END TESTING
 
 let notWantedUrls = [
   "https://www.youtube.com/shorts", 
@@ -27,23 +20,67 @@ function checkStartUrl(tab) {
   return false;
 }
 
-chrome.storage.sync.get(["running", "selectedTime"], (result) => {
+let timerId;
+let resetTimerId;
+
+function startTimer(tabId) {
+  console.log("Timer started!");
+  timerId = setTimeout(() => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["../content/content.js"]
+    });
+  }, selectedTime * 60 * 1000);
+}
+
+function stopTimer() {
+  if (timerId) {
+    clearTimeout(timerId);
+    timerId = null;
+    console.log("Timer stopped and reset!");
+  }
+}
+
+function startResetTimer() {
+  if (resetTimerId) {
+    clearTimeout(resetTimerId);
+  }
+  resetTimerId = setTimeout(() => {
+    console.log("Reset Timer started!");
+    stopTimer();
+  }, 10 * 60 * 1000);
+}
+
+chrome.storage.sync.get(["running", "selectedTime", "resetTimerTime"], (result) => {
   if (result.running !== undefined) settings.running = result.running;
   if (result.selectedTime !== undefined) settings.selectedTime = result.selectedTime;
+  if (result.resetTimerTime !== undefined) settings.resetTimerTime = result.resetTimerTime;
   
   if (settings.selectedTime === 0) settings.selectedTime = 0.2;
+  if (settings.resetTimerTime === 0) settings.resetTimerTime = 1;
 
   if (settings.running) {
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       if (changeInfo.status === "complete" && checkStartUrl(tab)) {
-        console.log("Timer started!");
-        setTimeout(() => {
-          chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ["../content/content.js"]
-          });
-        }, settings.selectedTime * 60 * 1000);
+        startTimer(tabId);
+        startResetTimer();
       }
+    });
+
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+      chrome.tabs.get(activeInfo.tabId, (tab) => {
+        if (checkStartUrl(tab)) {
+          startTimer(tab.id);
+          if (resetTimerId) {
+            clearTimeout(resetTimeoutId);
+            resetTimerId = null;
+          }
+        }
+        else {
+          stopTimer();
+          startResetTimer();
+        }
+      });
     });
   }
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,32 +1,45 @@
 let running = true;
 
 const btnTest = document.getElementById("btnTest");
+
 const btnOnOff = document.getElementById("btnOnOff");
+
 const timeSelect = document.getElementById("timeSelect");
 const btnValidate = document.getElementById("btnValidate");
+
+const resetTimerTime = document.getElementById("resetTimerTime");
+const btnResetTimer = document.getElementById("btnResetTimer");
+
+
+
+function updateButton() {
+  if (running) {
+    btnOnOff.innerText = "Running";
+    btnOnOff.classList.remove("bg-red-500");
+    btnOnOff.classList.add("bg-green-500");
+  }
+  else {
+    btnOnOff.innerText = "Not Running";
+    btnOnOff.classList.remove("bg-green-500");
+    btnOnOff.classList.add("bg-red-500");
+  }
+}
+
+chrome.storage.sync.get("running", (result) => {
+  if (result.running !== undefined) running = result.running;
+  updateButton();
+});
 
 btnTest.addEventListener("click", () => {
   console.log("Test!");
 });
 
 btnOnOff.addEventListener("click", () => {
-  if (running) {
-    btnOnOff.innerText = "Start";
-    btnOnOff.classList.remove("bg-green-500");
-    btnOnOff.classList.add("bg-red-500");
-  }
-  else {
-    btnOnOff.innerText = "Stop";
-    btnOnOff.classList.remove("bg-red-500");
-    btnOnOff.classList.add("bg-green-500");
-  }
   running = !running;
   if (chrome.storage && chrome.storage.sync) {
     chrome.storage.sync.set({ running: running }, () => {
-      if (running)
-        console.log("Extension Activated!");
-      else
-        console.log("Extension Deactivated!");
+      console.log("Running : ${running}");
+      updateButton();
     });
   }
 });
@@ -36,6 +49,15 @@ btnValidate.addEventListener("click", () => {
   if (chrome.storage && chrome.storage.sync) {
     chrome.storage.sync.set({ selectedTime: selectedTime }, () => {
       console.log("Selected time : ${selectedTime} minutes");
+    });
+  }
+});
+
+btnResetTimer.addEventListener("click", () => {
+  const resetTimerSelected = resetTimerTime.value;
+  if (chrome.storage && chrome.storage.sync) {
+    chrome.storage.sync.set({ resetTimerTime: resetTimerSelected }, () => {
+      console.log("Reset timer time : ${resetTimerSelected} minutes");
     });
   }
 });
